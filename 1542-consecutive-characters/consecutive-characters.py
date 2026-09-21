a05ce7df641089cc92a8bdafe0794d11845eb494
Uppala Lokesh <66313746+LOKESH-10-CRR@users.noreply.github.com>
2026-09-21 20:05:25 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (19.00%) - LeetSync

--- 1542-consecutive-characters/consecutive-characters.py
+++ 1542-consecutive-characters/consecutive-characters.py
@@ -1,13 +1,28 @@
 class Solution:
     def maxPower(self, s: str) -> int:
-        left, right, max_len, curr_len = 0, 0, 0, 0 
+        
+        ### Approach 1 with two pointers
+        
+        # left, right, max_len, curr_len = 0, 0, 0, 0 
+        # len_s = len(s)
+        # while right < len_s:
+        #     if s[left]==s[right]:
+        #         right+=1
+        #         curr_len+=1
+        #         max_len = max(max_len, curr_len)
+        #     elif s[left]!=s[right]:
+        #         curr_len = 0
+        #         left = right
+        # return max_len
+        
+        ### Approach 2 with single linear flow
+        curr_len, result = 1, 1
         len_s = len(s)
-        while right < len_s:
-            if s[left]==s[right]:
-                right+=1
+        for i in range(len_s-1):
+            if s[i]==s[i+1]:
                 curr_len+=1
-                max_len = max(max_len, curr_len)
-            elif s[left]!=s[right]:
-                curr_len = 0
-                left = right
-        return max_len     
+                result = max(result, curr_len)
+            else:
+                curr_len =1
+        return result
+
